610acc5ac407cb14ba7f1d9549621aed19897bd9
maks <maks1makrov@gmail.com>
2023-07-31 19:32:36 +0300

    all

--- src/catdog/tests.py
+++ src/catdog/tests.py
@@ -1,14 +1,17 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 from django.urls import reverse
 
 
 class TestCatDogView(TestCase):
+    def setUp(self) -> None:
+        self.client = Client()
+
 
     def test_get(self):
         response = self.client.get(reverse('catdog'))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, template_name='catdog.html')
-        self.assertEqual(len(self.client.session['data_for_session'].keys()), 3)
+        self.assertEqual(self.client.session['data_for_session']['species'], 3)
 
     def test_post_cat(self):
         response = self.client.post(reverse('catdog'),  {'cat': 'true'})
