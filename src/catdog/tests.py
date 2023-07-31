95d10db0888ae365304dbffefb44c9e6f33092c3
maks <maks1makrov@gmail.com>
2023-07-31 19:50:40 +0300

    all

--- src/catdog/tests.py
+++ src/catdog/tests.py
@@ -1,9 +1,11 @@
 from django.test import TestCase, Client
 from django.urls import reverse
 
+from catdog.models import AnimalImage
+
 
 class TestCatDogView(TestCase):
-    def setUp(self) -> None:
+    def setUp(self):
         self.client = Client()
 
 
@@ -11,17 +13,27 @@ class TestCatDogView(TestCase):
         response = self.client.get(reverse('catdog'))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, template_name='catdog.html')
-        self.assertEqual(self.client.session['data_for_session']['species'], 3)
+
 
     def test_post_cat(self):
         response = self.client.post(reverse('catdog'),  {'cat': 'true'})
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, template_name='pet.html')
+        self.assertEqual(self.client.session['data_for_session']['species'], 'cat')
+        self.assertEqual(len(self.client.session['data_for_session'].keys()), 3)
 
     def test_post_error(self):
         with self.assertRaisesMessage(AttributeError, "are you try to hack?"):
             self.client.post(reverse('catdog'),  {'boris': 'true'})
 
+
+    def test_save_catdog(self):
+        self.client.post(reverse('catdog'), {'cat': 'true'})
+        responce = self.client.post(reverse('save_catdog'),  {'cat': 'true'})
+        responce = self.client.post(reverse('save_catdog'),  {'cat': 'true'})
+        self.assertEqual(AnimalImage.objects.count(), 2)
+        self.assertTemplateUsed(responce, template_name='petsaved.html')
+        self.assert
 
 
 
